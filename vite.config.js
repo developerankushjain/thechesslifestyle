5cf6f75444922fce54e9ccda5c4a769981a0bc2c
Ankush Jain <teacherankushjain@gmail.com>
2026-04-17 15:10:08 +0530

    Add private packages page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        packages: resolve(__dirname, 'packages/index.html')
+      }
+    }
+  }
 })
