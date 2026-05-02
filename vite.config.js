d83644752cd287283ce633a8763a87634a7a0328
apple <apple@apples-MacBook-Air.local>
2026-05-02 16:16:54 +0530

    chore: Update SEO keywords and CTAs

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         packages: resolve(__dirname, 'packages/index.html'),
-        tournaments: resolve(__dirname, 'tournaments/index.html')
+        tournaments: resolve(__dirname, 'tournaments/index.html'),
+        chess_classes_noida: resolve(__dirname, 'chess-classes-noida/index.html'),
+        chess_home_tutor_noida: resolve(__dirname, 'chess-home-tutor-noida/index.html')
       }
     }
   }
